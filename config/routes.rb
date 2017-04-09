2623f0b6f763f53a6d8eb2b4159ea03f9d91ae48
Daniel <danielsfskim@gmail.com>
2017-04-09 00:31:27 +0000

    Emailing basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   devise_for :users
   devise_for :views
   root 'places#index'
   resources :places do
     resources :comments, only: :create
+    
   end
 end
