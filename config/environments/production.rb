3b673a78a77a04dd34c04f4951de6fa51f71f8a5
Daniel <danielsfskim@gmail.com>
2017-04-11 18:30:22 +0000

    e-mail config status

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,26 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net'
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
   }
+
+
+  # #ActionMailer::Base.smtp_settings = {
+  #   address: 'smtp.sendgrid.net'
+  #   port: '587',
+  #   authentication: :plain,
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: 'heroku.com',
+  #   enable_starttls_auto: true
+  # }
 
 
   config.action_mailer.default_url_options = { host: 'nomster2-daniel-kim.herokuapp.com' }
