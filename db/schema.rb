47b65f99290f0b94d606d71945b9158655509778
Daniel <danielsfskim@gmail.com>
2017-04-13 17:21:52 +0000

    deleted create_photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413021420) do
+ActiveRecord::Schema.define(version: 20170408000446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 20170413021420) do
     t.datetime "updated_at", null: false
     t.index ["place_id"], name: "index_comments_on_place_id", using: :btree
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id", using: :btree
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text     "caption"
-    t.integer  "place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -58,27 +51,8 @@ ActiveRecord::Schema.define(version: 20170413021420) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
-    t.string   "picture"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "views", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_views_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true, using: :btree
   end
 
 end
