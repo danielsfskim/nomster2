8e73facebb7741717c71c32ce71aaf9ee4f63f79
Daniel <danielsfskim@gmail.com>
2017-04-13 16:53:59 +0000

    rake drop

--- db/migrate/20170412030928_add_place_id_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddPlaceIdToPhotos < ActiveRecord::Migration[5.0]
-  def change
-    add_column :photos, :place_id, :integer
-  end
-end
--- db/migrate/20170413021420_alter_photos_fix_caption_typo.rb
+++
@@ -1,6 +0,0 @@
-class AlterPhotosFixCaptionTypo < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :photos, :cpation
-    add_column :photos, :caption, :text
-  end
-end
