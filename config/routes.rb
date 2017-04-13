64d67170d66870062807ba6bbe066bbac7ef3535
Daniel <danielsfskim@gmail.com>
2017-04-13 16:15:52 +0000

    cannot press add photo button

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
 
   devise_for :users
-  devise_for :views
   root 'places#index'
   resources :places do
     resources :comments, only: :create
