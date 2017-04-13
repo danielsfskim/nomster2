41b39f1d276cbeb257576e8769092470b589f88b
Daniel <danielsfskim@gmail.com>
2017-04-13 00:14:58 +0000

    typo fixes

--- db/migrate/20170412211743_add_captions_to_photos.rb
+++ db/migrate/20170412211743_add_captions_to_photos.rb
@@ -1,5 +1,5 @@
 class AddCaptionsToPhotos < ActiveRecord::Migration[5.0]
   def change
-    add_column :photos, :caption, :string #adding pictures to the photo column
+    add_column :photos, :caption, :text #adding pictures to the photo column
   end
 end
