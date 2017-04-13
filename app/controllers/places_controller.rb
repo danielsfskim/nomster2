34078abd616f2fcc805e873eb1bcba4a696e2538
Daniel <danielsfskim@gmail.com>
2017-04-13 03:50:37 +0000

    Finally

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -51,7 +51,6 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-
     @place.destroy
     redirect_to root_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,7 @@ ActiveRecord::Schema.define(version: 20170413021420) do
   create_table "photos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "picture"
-    t.string   "caption"
+    t.text     "caption"
     t.integer  "place_id"
   end
 
@@ -63,6 +62,23 @@ ActiveRecord::Schema.define(version: 20170413021420) do
     t.string   "picture"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_views_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true, using: :btree
   end
 
 end
